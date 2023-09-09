fcc880ac34509610bc5305ab1b3629bbbed38c9d
TheBigNameless <herzog.max@me.com>
2023-09-09 11:17:54 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int pruefungen;
+    int i = 1;
+    float ergebniss = 0;
+    int a = 0;
+    printf("Anzahl Pruefungen: ");
+    scanf("%d", &pruefungen);
+
+    while (pruefungen > i){
+        scanf ("%d\n", &a);
+        ergebniss = ergebniss + a;
+        printf("%f:",i);
+        printf(" %f",a);
+        printf(" %f\n",ergebniss);
+        a = 0;
+        i++;
+    }
+    printf("Ergebniss vor Rechnung:%f\n",ergebniss);
+    ergebniss = ergebniss/pruefungen;
+    printf("Ergebniss: %f", ergebniss);
+    return 0;
+}
